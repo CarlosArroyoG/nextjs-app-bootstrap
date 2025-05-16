630c55f18c521578ce1338c2dec3fb53f53c8085
Carlos Fernando Arroyo Garfias <110379162+CarlosArroyoG@users.noreply.github.com>
2025-05-16 00:43:14 -0600

    Add .next/static/chunks/src_app_reportes_page_tsx_955239ad._.js 1747377794421

---
+++ .next/static/chunks/src_app_reportes_page_tsx_955239ad._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6ad1c264._.js",
+  "static/chunks/src_29c42903._.js"
+],
+    source: "dynamic"
+});
