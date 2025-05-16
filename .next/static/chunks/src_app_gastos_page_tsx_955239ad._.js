e6471b8f3cd0e3317c897741058c9caa87917bc8
Carlos Fernando Arroyo Garfias <110379162+CarlosArroyoG@users.noreply.github.com>
2025-05-16 00:43:07 -0600

    Add .next/static/chunks/src_app_gastos_page_tsx_955239ad._.js 1747377787944

---
+++ .next/static/chunks/src_app_gastos_page_tsx_955239ad._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_aebab431._.js",
+  "static/chunks/src_63f03da4._.js"
+],
+    source: "dynamic"
+});
